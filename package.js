57b6dc6cc2ec9a1e1212a7ae6b73f9b5deae3d72
Robert Dickert <robert.dickert@gmail.com>
2014-08-08 10:50:06 -0600

    updates for Meteor 0.9 packaging system

--- package.js
+++ package.js
@@ -1,24 +1,27 @@
 Package.describe({
-  summary: "Reactive Velocity test reports in your app."
+  summary: "Reactive Velocity test reports in your app.",
+  version: "0.1.4",
+  git: "https://github.com/rdickert/velocity-html-reporter.git"
 })
 
 Package.on_use(function(api) {
-  api.use(['velocity', 'templating','amplify', 'less'], "client");
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
+  api.use(["xolvio:velocity@0.1.18", 'templating','amplify', 'less'], "client");
   
-  api.add_files('lib/reamplify.js', 'client');
+  api.addFiles('lib/reamplify.js', 'client');
 
-  api.add_files('lib/client-report.html', 'client');
-  api.add_files('lib/client-report.js', 'client');
-  api.add_files('lib/client-report.less', 'client');
-  api.add_files('lib/status-widget.less', 'client');
+  api.addFiles('lib/client-report.html', 'client');
+  api.addFiles('lib/client-report.js', 'client');
+  api.addFiles('lib/client-report.less', 'client');
+  api.addFiles('lib/status-widget.less', 'client');
 
   //bootstrap glyphicons
-  api.add_files('lib/bootstrap/src/fonts/glyphicons-halflings-regular.eot', 'client');
-  api.add_files('lib/bootstrap/src/fonts/glyphicons-halflings-regular.ttf', 'client');
-  api.add_files('lib/bootstrap/src/fonts/glyphicons-halflings-regular.woff', 'client');
-  api.add_files('lib/bootstrap/src/fonts/glyphicons-halflings-regular.svg', 'client');
+  api.addFiles('lib/bootstrap/src/fonts/glyphicons-halflings-regular.eot', 'client');
+  api.addFiles('lib/bootstrap/src/fonts/glyphicons-halflings-regular.ttf', 'client');
+  api.addFiles('lib/bootstrap/src/fonts/glyphicons-halflings-regular.woff', 'client');
+  api.addFiles('lib/bootstrap/src/fonts/glyphicons-halflings-regular.svg', 'client');
 
-  api.add_files('lib/spinner.gif');
+  api.addFiles('lib/spinner.gif');
 
   // api.export('reamplify', ['client']);
 });
